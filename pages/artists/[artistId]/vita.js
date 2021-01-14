08d21b4b85b2a9fcd953759a581808595204e9e7
Nicolas <nicolas.koelmel@icloud.com>
2021-01-14 15:52:12 +0100

    render vita at build time

--- pages/artists/[artistId]/vita.js
+++ pages/artists/[artistId]/vita.js
@@ -4,9 +4,9 @@ import Header from '../../../components/Header'
 import Footer from '../../../components/Footer'
 import HTMLHead from '../../../components/HTMLHead'
 
-const Vita = () => {
-	const { query } = useRouter()
-	const artist = query.artist ? JSON.parse(query.artist) : {}
+const BACKEND_URI = process.env.NEXT_PUBLIC_BACKEND_URI
+
+const Vita = ({ artist }) => {
 
   return (
     <div className={styles.container}>
@@ -22,3 +22,26 @@ const Vita = () => {
 }
 
 export default Vita
+
+// This function gets called at build time
+export async function getStaticPaths() {
+  // Call an external API endpoint to get posts
+  const res = await fetch(BACKEND_URI+`artists?$limit=10000`)
+  const artists = await res.json()
+  // Get the paths we want to pre-render based on artists
+  const paths = artists.data.map((artist) => `/artists/${artist._id}/vita`)
+  // We'll pre-render only these paths at build time.
+  // { fallback: false } means other routes should 404.
+  return { paths, fallback: false }   
+  
+}
+
+// This also gets called at build time
+export async function getStaticProps({ params }) {
+  // params contains the artist `id`.
+  // If the route is like /artist/1, then params.id is 1
+  const resArtist = await fetch(BACKEND_URI+`artists/${params.artistId}`)
+  const artist = await resArtist.json()
+  // Pass artist data to the page via props
+  return { props: { artist } }
+}
