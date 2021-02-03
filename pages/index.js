b075102e1596536bc7ff0896dbc7da9a80474a53
Nicolas <nicolas.koelmel@icloud.com>
2021-02-03 16:12:54 +0100

    contact info on main page

--- pages/index.js
+++ pages/index.js
@@ -13,6 +13,11 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to Werkliste
         </h1>
+        <div className={styles.advertisement}>
+          <p>Publish your own website with unlimited storage. Manage your artworks in the cloud. Plans starting at 5 EUR/month.</p>
+          <p>Check out live examples from other artists like: <a href="http://www.hannesmichanek.com/" target="_blank">Hannes Michanek</a></p>
+          <p><a href="mailto:kontakt@werkliste.ch?subject=Request more information about Werkliste&body=Hi%20there.">-Contact us for more information-</a></p>
+        </div>
       </main>
 
       <footer className={styles.footer}>
