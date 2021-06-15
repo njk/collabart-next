733ba1b8c4cc2141c22b74daab51a1c753fc7148
Nicolas <nicolas.koelmel@icloud.com>
2021-06-16 00:07:19 +0200

    new menu layout

--- components/Header.js
+++ components/Header.js
@@ -4,21 +4,18 @@ import styles from '../styles/Home.module.css'
 const Header = ({artist, menuItems}) => {
 
   return (<header className={styles.header}>
-      <ul>
-        {menuItems.includes("works") && <li>
-          <Link href={"/artists/"+artist._id}>
-            <a>Works</a>
-          </Link>
-        </li>}
-        {menuItems.includes("vita") && <li>
-          <Link href={{
+      <span>
+        <Link href={"/artists/"+artist._id}>
+          <a className={menuItems.includes("vita") && styles.headerLinkActive} ><b>Works</b></a>
+        </Link>
+        <span> - </span>
+        <Link href={{
             pathname: "/artists/"+artist._id+"/vita",
             query: { artist: JSON.stringify(artist) }
           }} as={`/artists/${artist._id}/vita`}>
-            <a>Vita</a>
-          </Link>
-        </li>}
-      </ul>
+          <a className={menuItems.includes("works") && styles.headerLinkActive}><b>Vita</b></a>
+        </Link>
+      </span>
     </header>)
 }
 
