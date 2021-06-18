03752173dd1890f2e12bec18141ca3fe8cc33abb
Nicolas <nicolas.koelmel@icloud.com>
2021-06-18 22:50:24 +0200

    fix conditional styling

--- components/Header.js
+++ components/Header.js
@@ -6,14 +6,14 @@ const Header = ({artist, menuItems = []}) => {
   return (<header className={styles.header}>
       <span>
         <Link href={"/artists/"+artist._id}>
-          <a className={menuItems.includes("vita") && styles.headerLinkActive} ><b>Works</b></a>
+          <a className={menuItems.includes("vita") ? styles.headerLinkActive : undefined} ><b>Works</b></a>
         </Link>
         <span> - </span>
         <Link href={{
             pathname: "/artists/"+artist._id+"/vita",
             query: { artist: JSON.stringify(artist) }
           }} as={`/artists/${artist._id}/vita`}>
-          <a className={menuItems.includes("works") && styles.headerLinkActive}><b>Vita</b></a>
+          <a className={menuItems.includes("works") ? styles.headerLinkActive : undefined}><b>Vita</b></a>
         </Link>
       </span>
     </header>)
