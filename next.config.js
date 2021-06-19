e30b1f8dc70036ebdbe8eb9f69cb142764ea601e
Nicolas <nicolas.koelmel@icloud.com>
2021-06-19 02:20:57 +0200

    redirect sabinekoelmel and hannesmichanek

--- next.config.js
+++ next.config.js
@@ -4,20 +4,33 @@ module.exports = {
     deviceSizes: [1080, 2048],
     imageSizes: [1024, 2048]
   },
-  async rewrites() {
+  async redirects() {
     return [
       // if the host is `sabinekoelmel.com`,
       // this rewrite will be applied
       {
-        source: '/:path*',
+        source: '/',
         has: [
           {
             type: 'host',
             value: 'sabinekoelmel.com',
           },
         ],
+        permanent: false,
         destination: '/artists/5bab7f495d212a93e8cdd748',
-      }
-    ]
-  },
+      },
+      // if the host is `hannesmichanek.com`,
+      // this rewrite will be applied
+      {
+        source: '/',
+        has: [
+          {
+            type: 'host',
+            value: 'hannesmichanek.com',
+          },
+        ],
+        permanent: false,
+        destination: '/artists/5bab7f495d212a93e8cdd6ee',
+      }]
+  }
 }
